e233a387dcdd0eedfcd64f266fd7c7b1c1eb4c41
SusannaOlivero <131966758+SusannaOlivero@users.noreply.github.com>
2023-12-01 22:43:25 -0600

    Update dream_flute.py

--- DREAM/dream_flute.py
+++ DREAM/dream_flute.py
@@ -1,1 +1,36 @@
-import
+import torch
+import transformers
+import numpy as np
+import random
+import json
+
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
+
+device = torch.device('cuda')
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+model = AutoModelForSeq2SeqLM.from_pretrained("allenai/System1_FigLang2022", torch_dtype=torch.float16, device_map=device)
+tokenizer = AutoTokenizer.from_pretrained("t5-3b", torch_dtype=torch.float16, device_map=device)
+
+with open("home/solivero/FLUTE_data/FLUTE_test.json") as f:
+    data = json.load(f)
+
+for i in range(len(data)):
+    premise = data[i]["premise"]
+    hypothesis = data[i]["hypothesis"]
+    input_string = "Premise: "+premise+" Hypothesis: "+hypothesis+". Is there a contradiction or entailment between the premise and hypothesis?"
+    input_ids = tokenizer.encode(input_string, return_tensors="pt")
+    output = model.generate(input_ids, max_length=200)
+    text = tokenizer.batch_decode(output, skip_special_tokens=True)
+    #["Answer : Contradiction. Explanation : Most people would not be happy to see someone else's new car that they cannot afford because it is way out of their budget"]
+    label = text.split("Explanation")[0].lstrip()
+    label = label.split("Answer :")[1].lstrip()
+    data[i]["predicted_label"] = label
+    explanation = text.split("Explanation")[1].lstrip()
+    data[i]["model_explanation"] = explanation
+
+with open("home/solivero/dream_flute_system1.json","w") as f:
+    f.write(json.dumps(data,indent=4))
