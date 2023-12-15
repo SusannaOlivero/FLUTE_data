8f163f57ace6c79f649e8f2d3f667fe2ac95d0af
SusannaOlivero <131966758+SusannaOlivero@users.noreply.github.com>
2023-12-15 13:26:20 +0100

    Update predictions_T0_K24_b.py

--- predictions_T0_K24_b.py
+++ predictions_T0_K24_b.py
@@ -16,7 +16,7 @@ model_tag = "meta-llama/Llama-2-7b-chat-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_tag, token=MY_TOKEN)
 model = AutoModelForCausalLM.from_pretrained(model_tag, token=MY_TOKEN, torch_dtype=torch.float16, device_map=device)
 
-with open("home/solivero/FLUTE_data/FLUTE_train.json") as f:
+with open("FLUTE_data/FLUTE_train.json") as f:
     data_train = json.load(f)
 
 k = 4
@@ -81,7 +81,7 @@ tokens = tokenizer.tokenize(str(examples))
 token_count = len(tokens)
 length_max = token_count + 150
 
-with open("home/solivero/FLUTE_data/FLUTE_val.json") as f:
+with open("FLUTE_data/FLUTE_val.json") as f:
     data = json.load(f)
 
 for i in range(len(data)):
@@ -106,5 +106,5 @@ for i in range(len(data)):
     data[i]["predicted_label"] = predictedlabel
     data[i]["model_explanation"] = explanation_
 
-with open("home/solivero/prediction_t0_k24_b.json","w") as f:
+with open("prediction_t0_k24_b.json","w") as f:
     f.write(json.dumps(data,indent=4))
