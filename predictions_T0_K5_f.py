5a18323b7a6b462bbda774c6dee52dac71540620
SusannaOlivero <131966758+SusannaOlivero@users.noreply.github.com>
2023-12-22 19:02:51 +0100

    Update predictions_T0_K5_f.py

--- predictions_T0_K5_f.py
+++ predictions_T0_K5_f.py
@@ -16,7 +16,7 @@ model_tag = "meta-llama/Llama-2-7b-chat-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_tag, use_auth_token=MY_TOKEN)
 model = AutoModelForCausalLM.from_pretrained(model_tag, use_auth_token=MY_TOKEN, torch_dtype=torch.float16, device_map=device)
 
-with open("FLUTE_data/FLUTE_train.json") as f:
+with open("FLUTE_data/FLUTE_train_2.json") as f:
     data_train = json.load(f)
 
 k = 5
@@ -124,7 +124,7 @@ tokens = tokenizer.tokenize(str(examples))
 token_count = len(tokens)
 length_max = token_count + 150
 
-with open("FLUTE_data/FLUTE_val.json") as f:
+with open("FLUTE_data/FLUTE_val_2.json") as f:
     data = json.load(f)
 
 for i in range(len(data)):
@@ -136,7 +136,7 @@ for i in range(len(data)):
     request = "\npremise: "+premise+"\nhypothesis: "+hypothesis
     prompt += request
     inputs = tokenizer(prompt, return_token_type_ids=False, return_tensors="pt").to(device)
-    generate_ids = model.generate(**inputs, do_sample=True, temperature=0.3, top_k=10, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, max_length=length_max)
+    generate_ids = model.generate(**inputs, do_sample=False, temperature=0, top_k=10, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, max_length=length_max)
     text = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
     text = text.replace(prompt, '').strip()
     label_ = text.split("Explanation:")[0].lstrip()
@@ -149,5 +149,5 @@ for i in range(len(data)):
     data[i]["predicted_label"] = predictedlabel
     data[i]["model_explanation"] = explanation_
 
-with open("prediction_t03_k5_f.json","w") as f:
+with open("prediction2_t0_k5_f.json","w") as f:
     f.write(json.dumps(data,indent=4))
