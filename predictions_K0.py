3a55ce81e3a41bb3e226908787ba8a6018944550
SusannaOlivero <131966758+SusannaOlivero@users.noreply.github.com>
2024-01-03 12:41:01 +0100

    Update predictions_K0.py

--- predictions_K0.py
+++ predictions_K0.py
@@ -26,7 +26,7 @@ for i in range(len(data)):
     prompt = "Find if the 'premise' entails or contradicts the 'hypothesis'.\nThe output must strictly follow the following format: Label: 'Entails' or 'Contradicts', Explanation: 'text'"
     request = "\npremise: "+premise+"\nhypothesis: "+hypothesis
     prompt += request
-    inputs = tokenizer(prompt, return_tensors="pt").to(device)
+    inputs = tokenizer(prompt, return_token_type_ids=False, return_tensors="pt").to(device)
     generate_ids = model.generate(**inputs, do_sample=False, temperature=0, top_k=10, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, max_length=256)
     text = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
     text = text.replace(prompt, '').strip()
@@ -66,7 +66,7 @@ for i in range(len(data)):
     prompt = "Find if the 'premise' entails or contradicts the 'hypothesis'.\nThe output must strictly follow the following format: Label: 'Entails' or 'Contradicts', Explanation: 'text'"
     request = "\npremise: "+premise+"\nhypothesis: "+hypothesis
     prompt += request
-    inputs = tokenizer(prompt, return_tensors="pt").to(device)
+    inputs = tokenizer(prompt, return_token_type_ids=False, return_tensors="pt").to(device)
     generate_ids = model.generate(**inputs, do_sample=True, temperature=0.3, top_k=10, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, max_length=256)
     text = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
     text = text.replace(prompt, '').strip()
