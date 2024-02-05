590112d9e33872c0b84e9a0b50ac54498c6bcce0
SusannaOlivero <131966758+SusannaOlivero@users.noreply.github.com>
2024-02-05 17:47:40 +0100

    Update pred_single_class_K10.py

--- pred_single_class_K10.py
+++ pred_single_class_K10.py
@@ -16,113 +16,164 @@ model_tag = "meta-llama/Llama-2-7b-chat-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_tag, use_auth_token=MY_TOKEN)
 model = AutoModelForCausalLM.from_pretrained(model_tag, use_auth_token=MY_TOKEN, torch_dtype=torch.float16, device_map=device)
 
+k = 10
+
 with open("FLUTE_data/FLUTE_train.json") as f:
     data_train = json.load(f)
 
-k = 10
-method = 'random'
+data_train_metaphor = []
+data_train_idiom = []
+data_train_sarcasm = []
+data_train_simile = []
+for data in data_train:
+    if data['data_type'] == 'metaphor':
+        data_train_metaphor.append(data)
+    elif data['data_type'] == 'idiom':
+        data_train_idiom.append(data)
+    elif data['data_type'] == 'sarcasm':
+        data_train_sarcasm.append(data)
+    elif data['data_type'] == 'simile':
+        data_train_simile.append(data)
+del data_train
 
-input_data = []
+# 1. METAPHOR
+input_data_metaphor = []
+input_data += random.sample(data_train_metaphor, k-1)
 
-if k == 2:
-    data_entail = []
-    data_contra = []
-    for data in data_train:
-        if data['label'] == 'Entailment':
-            data_entail.append(data)
-        elif data['label'] == 'Contradiction':
-            data_contra.append(data)
-    input_data += random.sample(data_entail, 1)
-    input_data += random.sample(data_contra, 1)
-
-    del data_entail
-    del data_contra
-
-elif method == 'figure' and k > 3:
-    k_type = k // 4
-    k_random = k - (k_type * 4)
-
-    data_train_metaphor = []
-    data_train_idiom = []
-    data_train_sarcasm = []
-    data_train_simile = []
-    for data in data_train:
-        if data['data_type'] == 'metaphor':
-            data_train_metaphor.append(data)
-        elif data['data_type'] == 'idiom':
-            data_train_idiom.append(data)
-        elif data['data_type'] == 'sarcasm':
-            data_train_sarcasm.append(data)
-        elif data['data_type'] == 'simile':
-            data_train_simile.append(data)
-
-    input_data += random.sample(data_train_metaphor, k_type)
-    input_data += random.sample(data_train_idiom, k_type)
-    input_data += random.sample(data_train_simile, k_type)
-    input_data += random.sample(data_train_sarcasm, k_type - 1)
-
-    entailment_list = [item for item in input_data if item['label'] == 'Entailment']
-    contradiction_list = [item for item in input_data if item['label'] == 'Contradiction']
-    if not entailment_list:  # if it's empty
-        entailment_data_sarc = [item for item in data_train_sarcasm if item['label'] == 'Entailment']
-        input_data += random.sample(entailment_data_sarc, 1)
-        del entailment_data_sarc
-    elif not contradiction_list:
-        contradiction_data_sarc = [item for item in data_train_sarcasm if item['label'] == 'Contradiction']
-        input_data += random.sample(contradiction_data_sarc, 1)
-        del contradiction_data_sarc
-    else:
-        input_data += random.sample(data_train_sarcasm, 1)
-
-    if k_random != 0:
-        i = 0
-        while i < k_random:
-            prova = random.sample(data_train, 1)
-            if prova not in input_data:
-                input_data += prova
-                i += 1
-
-    del data_train_metaphor
-    del data_train_idiom
-    del data_train_sarcasm
-    del data_train_simile
-
-# method = 'random'
+entailment_list = [item for item in input_data if item['label'] == 'Entailment']
+contradiction_list = [item for item in input_data if item['label'] == 'Contradiction']
+if not entailment_list:  # if it's empty
+    entailment_data_ = [item for item in data_train_metaphor if item['label'] == 'Entailment']
+    input_data_metaphor += random.sample(entailment_data_, 1)
+    del entailment_data_
+elif not contradiction_list:
+    contradiction_data_ = [item for item in data_train_metaphor if item['label'] == 'Contradiction']
+    input_data_metaphor += random.sample(contradiction_data_, 1)
+    del contradiction_data_
 else:
-    input_data += random.sample(data_train, k - 1)
+    input_data_metaphor += random.sample(data_train_metaphor, 1)
 
-    entailment_list = [item for item in input_data if item['label'] == 'Entailment']
-    contradiction_list = [item for item in input_data if item['label'] == 'Contradiction']
-    if not entailment_list:
-        entailment_data = [item for item in data_train if item['label'] == 'Entailment']
-        input_data += random.sample(entailment_data, 1)
-        del entailment_data
-    elif not contradiction_list:
-        contradiction_data = [item for item in data_train if item['label'] == 'Contradiction']
-        input_data += random.sample(contradiction_data, 1)
-        del contradiction_data
-    else:
-        input_data += random.sample(data_train, 1)
+del data_train_metaphor
 
-output_text = ""
-for item in input_data:
-    output_text += f"\npremise: {item['premise']}\n"
-    output_text += f"hypothesis: {item['hypothesis']}\n"
+output_text_metaphor = ""
+for item in input_data_metaphor:
+    output_text_metaphor += f"\npremise: {item['premise']}\n"
+    output_text_metaphor += f"hypothesis: {item['hypothesis']}\n"
     label = item['label']
     if "Entailment" in label:
         answer = "Entails."
     elif "Contradiction" in label:
         answer = "Contradicts."
-    output_text += f"Answer: {answer}\n"
-    output_text += f"Explanation: {item['explanation']}\n"
+    output_text_metaphor += f"Answer: {answer}\n"
+    output_text_metaphor += f"Explanation: {item['explanation']}\n"
 
-del data_train
 
-examples = output_text
+# 2. IDIOM
+input_data_idiom = []
+input_data += random.sample(data_train_idiom, k-1)
 
-tokens = tokenizer.tokenize(str(examples))
-token_count = len(tokens)
-length_max = token_count + 210
+entailment_list = [item for item in input_data if item['label'] == 'Entailment']
+contradiction_list = [item for item in input_data if item['label'] == 'Contradiction']
+if not entailment_list:  # if it's empty
+    entailment_data_ = [item for item in data_train_idiom if item['label'] == 'Entailment']
+    input_data_idiom += random.sample(entailment_data_, 1)
+    del entailment_data_
+elif not contradiction_list:
+    contradiction_data_ = [item for item in data_train_idiom if item['label'] == 'Contradiction']
+    input_data_idiom += random.sample(contradiction_data_, 1)
+    del contradiction_data_
+else:
+    input_data_idiom += random.sample(data_train_idiom, 1)
+
+del data_train_idiom
+
+output_text_idiom = ""
+for item in input_data_idiom:
+    output_text_idiom += f"\npremise: {item['premise']}\n"
+    output_text_idiom += f"hypothesis: {item['hypothesis']}\n"
+    label = item['label']
+    if "Entailment" in label:
+        answer = "Entails."
+    elif "Contradiction" in label:
+        answer = "Contradicts."
+    output_text_idiom += f"Answer: {answer}\n"
+    output_text_idiom += f"Explanation: {item['explanation']}\n"
+
+
+# 3. SIMILE
+input_data_simile = []
+input_data += random.sample(data_train_simile, k-1)
+
+entailment_list = [item for item in input_data if item['label'] == 'Entailment']
+contradiction_list = [item for item in input_data if item['label'] == 'Contradiction']
+if not entailment_list:  # if it's empty
+    entailment_data_ = [item for item in data_train_simile if item['label'] == 'Entailment']
+    input_data_simile += random.sample(entailment_data_, 1)
+    del entailment_data_
+elif not contradiction_list:
+    contradiction_data_ = [item for item in data_train_simile if item['label'] == 'Contradiction']
+    input_data_simile += random.sample(contradiction_data_, 1)
+    del contradiction_data_
+else:
+    input_data_simile += random.sample(data_train_simile, 1)
+
+del data_train_simile
+
+output_text_simile = ""
+for item in input_data_simile:
+    output_text_simile += f"\npremise: {item['premise']}\n"
+    output_text_simile += f"hypothesis: {item['hypothesis']}\n"
+    label = item['label']
+    if "Entailment" in label:
+        answer = "Entails."
+    elif "Contradiction" in label:
+        answer = "Contradicts."
+    output_text_simile += f"Answer: {answer}\n"
+    output_text_simile += f"Explanation: {item['explanation']}\n"
+
+
+# 4. SARCASM
+input_data_sarcasm = []
+input_data += random.sample(data_train_sarcasm, k-1)
+
+entailment_list = [item for item in input_data if item['label'] == 'Entailment']
+contradiction_list = [item for item in input_data if item['label'] == 'Contradiction']
+if not entailment_list:  # if it's empty
+    entailment_data_ = [item for item in data_train_sarcasm if item['label'] == 'Entailment']
+    input_data_sarcasm += random.sample(entailment_data_, 1)
+    del entailment_data_
+elif not contradiction_list:
+    contradiction_data_ = [item for item in data_train_sarcasm if item['label'] == 'Contradiction']
+    input_data_sarcasm += random.sample(contradiction_data_, 1)
+    del contradiction_data_
+else:
+    input_data_sarcasm += random.sample(data_train_sarcasm, 1)
+
+del data_train_sarcasm
+
+output_text_sarcasm = ""
+for item in input_data_sarcasm:
+    output_text_sarcasm += f"\npremise: {item['premise']}\n"
+    output_text_sarcasm += f"hypothesis: {item['hypothesis']}\n"
+    label = item['label']
+    if "Entailment" in label:
+        answer = "Entails."
+    elif "Contradiction" in label:
+        answer = "Contradicts."
+    output_text_sarcasm += f"Answer: {answer}\n"
+    output_text_sarcasm += f"Explanation: {item['explanation']}\n"
+
+
+tokens_metaphor = tokenizer.tokenize(str(output_text_metaphor))
+token_count_metaphor = len(tokens_metaphor)
+tokens_idiom = tokenizer.tokenize(str(output_text_idiom))
+token_count_idiom = len(tokens_idiom)
+tokens_simile = tokenizer.tokenize(str(output_text_simile))
+token_count_simile = len(tokens_simile)
+tokens_sarcasm = tokenizer.tokenize(str(output_text_sarcasm))
+token_count_sarcasm = len(tokens_sarcasm)
+token_count = max(token_count_metaphor, token_count_idiom, token_count_simile, token_count_sarcasm)
+length_max = token_count + 220
 
 with open("FLUTE_data/FLUTE_test.json") as f:
     data = json.load(f)
@@ -132,6 +183,7 @@ for i in range(len(data)):
     hypothesis = data[i]["hypothesis"]
     prompt = "Find if the 'premise' entails or contradicts the 'hypothesis'.\n"
     prompt += "Here you can find some examples of answers:\n"
+    if data[i][]
     prompt += examples
     request = "\npremise: "+premise+"\nhypothesis: "+hypothesis
     prompt += request
